54461b4f0fbd5f7ac238e0dbf0ceda4ca0e1e933
Harini-win <harini.selvakummar@gmail.com>
2025-06-11 12:36:56 +0530

    updated techstack section for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -203,21 +203,19 @@ useEffect(() => {
    
     <PortfolioHero taglines={taglines} Typewriter={Typewriter} />
     <ProjectsSection />
-<section id="skill" className="py-20 px-8">
+<section id="skill" className="py-20 px-4 sm:px-8">
   <div className="max-w-7xl mx-auto">
-    <h2 className="text-center text-5xl font-bold mb-16 relative z-20">Tech Stacks</h2>
-    
-    <div className="grid grid-cols-12 gap-4">
+    <h2 className="text-center text-3xl sm:text-5xl font-bold mb-16 relative z-20">Tech Stacks</h2>
+    <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 gap-4">
       {techStack.map((tech, index) => (
         <div
           key={index}
           className="group rounded-2xl p-2 text-center"
         >
-     
           <img 
             src={tech} 
             alt="Tech Logo"
-            className="w-20 h-20 mx-auto mb-4 object-contain bg-white rounded-lg p-2 cursor-pointer transition-all duration-300 filter brightness-50 contrast-70 grayscale group-hover:brightness-100 group-hover:contrast-100 group-hover:grayscale-0"
+            className="w-16 h-16 sm:w-20 sm:h-20 mx-auto mb-4 object-contain bg-white rounded-lg p-2 cursor-pointer transition-all duration-300 filter brightness-50 contrast-70 grayscale group-hover:brightness-100 group-hover:contrast-100 group-hover:grayscale-0"
           />
         </div>
       ))}
